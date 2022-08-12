8dfe9b138939e41601a839d4c680825b570eac6a
SylviaYang2 <sylviayang252@berkeley.edu>
2022-08-12 00:12:13 -0700

    Update mongoose connection and add node version

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true })
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("MONGO CONNECTION OPEN!!!")
     })
